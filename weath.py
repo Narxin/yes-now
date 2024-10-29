be1243a114a52a44a6bfd59086c4435b613a47cd
Vitalij <belikovvitalij16@gmail.com>
2024-10-29 19:44:55 +0200

    v11

--- weath.py
+++ weath.py
@@ -7,6 +7,8 @@ city = input('''Напишите название города:
 res = requests.get('https://api.openweathermap.org/data/2.5/weather', params= {'q' : city, 'units' : 'metric', 'lang' : 'RU', 'APPID' : syek.key})
 data = res.json()
 
+
+
 print('Город:', city)
 print('Погодные условия:', data['weather'][0]['description'])
 print('Температура:', data['main']['temp'])
